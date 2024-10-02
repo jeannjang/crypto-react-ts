c459b392d0111994041955d97cf5102d4e39c2fa
Jean <jeanjyjh@gmail.com>
2024-10-02 18:58:57 +1300

    스타일 컴포넌트 링크 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import styled from "styled-components";
 
+const Father = styled.div`
+  display: flex;
+`;
+const Btn = styled.button`
+  background-color: ${(props) => props.bgColor};
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  margin: 10px;
+`;
+
 function App() {
   return (
-    <div>
-      <h1> Hello World </h1>
-    </div>
+    <Father>
+      <Btn bgColor="red">버튼1</Btn>
+      <Btn as="a" href="/" bgColor="blue">
+        버튼2
+      </Btn>
+    </Father>
   );
 }
 
