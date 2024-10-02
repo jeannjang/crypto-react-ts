23e03402c6c68c35aed3872af95197fe4f730b52
Jean <jeanjyjh@gmail.com>
2024-10-02 19:01:11 +1300

    attrs( 애트리뷰츠) 컴포넌트 활용

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Btn = styled.button`
-  background-color: ${(props) => props.bgColor};
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  margin: 10px;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Btn bgColor="red">버튼1</Btn>
-      <Btn as="a" href="/" bgColor="blue">
-        버튼2
-      </Btn>
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
