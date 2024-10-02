f2b6382e7d3032099ce70ccbf5d37843cb474d0f
Jean <jeanjyjh@gmail.com>
2024-10-02 20:23:42 +1300

    스타일 컴포넌트 Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Animation = keyframes`
@@ -11,17 +14,31 @@ const Animation = keyframes`
   100% { transform: scale(1); }
   `;
 
+const Emoji = styled.span`
+  font-size: 40px;
+`;
+
 const Box = styled.div`
   width: 100px;
   height: 100px;
   background-color: salmon;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   animation: ${Animation} 2s infinite;
+  ${Emoji} {
+    &:hover {
+      opacity: 0.5;
+    }
+  }
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box></Box>
+      <Box>
+        <Emoji>👀</Emoji>
+      </Box>
     </Wrapper>
   );
 }
