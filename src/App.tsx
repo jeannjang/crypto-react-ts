edc4d3f0e72510484c7b63fe0f8c4b1357a5d20b
Jean <jeanjyjh@gmail.com>
2024-10-06 21:22:33 +1300

    Use state with form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import Circle from "./Circle";
+import { useState } from "react";
+import React from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`Welcome, ${value}`);
+  };
   return (
     <div>
-      <Circle bgColor="salmon" borderColor="red" />
-      <Circle bgColor="green" text="optinal" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          type="text"
+          placeholder="Username"
+          onChange={onChange}
+        />
+        <button type="submit">Log in</button>
+      </form>
     </div>
   );
 }
